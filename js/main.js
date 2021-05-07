515ab8818c806e97a634c6c0396c07e9d1c97fd7
MElmenofy <53525215+MElmenofy@users.noreply.github.com>
2021-05-07 09:11:31 +0200

    m

--- js/main.js
+++ js/main.js
@@ -16,5 +16,25 @@ $(document).ready(function(){
           prevEl: '.swiper-button-prev',
         },
       });
+
+      (function($) {
+        "use strict";
+    
+    
+      /* ********************************************
+        5. Countdown
+      ******************************************** */
+        $('[data-countdown]').countdown('2020/12/20', function(event) {
+            $(this).html(
+          event.strftime(
+            '<span class="cdown days"><span class="time-count">%-D</span> <p>Days</p></span> <span class="cdown hour"><span class="time-count">%-H</span> <p>Hour</p></span> <span class="cdown minutes"><span class="time-count">%M</span> <p>Mint</p></span> <span class="cdown second"> <span><span class="time-count">%S</span> <p>Sec</p></span>'
+          )
+        );
+        }); 
+    
+    
+    })(jQuery);
+    
+    
    
 })
